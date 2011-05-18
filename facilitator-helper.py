f0c6d2914fe297127a947ef21d165c95b424040a
Emily Stark <estark37@gmail.com>
2011-05-17 22:07:28 -0700

    updated facilitator helper to work with flash ads

--- facilitator-helper.py
+++ facilitator-helper.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 import storage
-from storage import AmazonSQS
+from storage import *
 import time
 import httplib
 import urllib
 
-DEFAULT_FACILITATOR = "173.255.221.44"
+DEFAULT_FACILITATOR = "127.0.0.1"
 DEFAULT_PORT = 9002
 
 def post_client(client):
-    http = httplib.HTTPConnection(DEFAULT_FACILITATOR)
+    http = httplib.HTTPConnection(DEFAULT_FACILITATOR, DEFAULT_PORT)
     print "Posting client: %s"%client
     http.request("POST", "/", urllib.urlencode({"client": client}))
     http.close()
@@ -24,7 +24,7 @@ def poll():
         if (client):
             print "Found a client: %s"%client
             post_client(client)
-        time.sleep(1)
+        time.sleep(5)
 
 if __name__ == '__main__':
     poll()
